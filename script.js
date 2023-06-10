c7d1f8305a17b9d7ccd9c0508763439ab8a73b82
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-10 10:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function volume_sphere() {
     //Write your code here
+	let inputrad = document.getElementById('radius');
+	let inputvol =document.getElementById('volume');
+	let rad= parseFloat(inputrad.value);
+	const pi= Math.PI;
+	let volume=(4 / 3) * pi * Math.pow(rad, 3);
+	inputvol.value=volume;
   
 } 
 
